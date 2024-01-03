0e9b435b097bf7d9913df4a0108d9c1bdb103f5b
Sumit Raj <69194685+sumitraj906@users.noreply.github.com>
2024-01-03 23:09:12 +0530

    Update PostList.jsx

--- Socialmedia/src/assets/component/PostList.jsx
+++ Socialmedia/src/assets/component/PostList.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext ,useEffect } from "react";
 import { PostList as PostListData } from "../store/post-list-store";
 import Post from "./Post";
 import WelcomeMessage from "./WelcomeMessage";
@@ -6,16 +6,18 @@ import WelcomeMessage from "./WelcomeMessage";
 const PostList=()=>{
   const{postList,addInitialPosts} = useContext(PostListData);
 
-   const handleGetPostClick=()=>{
+   useEffect(()=>{
     fetch("https://dummyjson.com/posts")
     .then((res) => res.json())
     .then(data=>{
       addInitialPosts(data.posts);
     });
-   };
+  },[]);
+  
+   
     return(
     <>
-    {postList.length===0 && (<WelcomeMessage onGetPostClick={handleGetPostClick}/>
+    {postList.length===0 && (<WelcomeMessage />
     )}
     {postList.map((post)=>(
         <Post key={post.id} post={post}/>
